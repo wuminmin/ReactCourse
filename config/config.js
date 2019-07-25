9fa8a408bf61dbe8e98d21b20397475852d6c4b1
admin <you@example.com>
2019-07-25 22:34:07 +0800

    新用户注册register

--- config/config.js
+++ config/config.js
@@ -2,10 +2,18 @@ export default {
     plugins: [
         ['umi-plugin-react', {
           // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+          antd: true
+
         }],
       ],
-    routes: [{
+    routes: [
+      {
         path: '/',
-        component: './HelloWorld',
-      }],
+        component: './index',
+      },
+      {
+        path: '/register',
+        component: './register',
+      },
+    ],
 };
