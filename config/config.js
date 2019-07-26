a2192af31c61cdaab53f7f4729e05bef62a4985d
admin <you@example.com>
2019-07-26 11:18:11 +0800

    1

--- config/config.js
+++ config/config.js
@@ -1,23 +1,34 @@
 export default {
-    plugins: [
-        ['umi-plugin-react', {
-          // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-          antd: true
+  plugins: [
+    ['umi-plugin-react', {
+      // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+      antd: true,
+      dva:true
+    }],
+  ],
 
-        }],
-      ],
-    routes: [
-      {
-        path: '/',
-        component: './index',
-      },
-      {
-        path: '/register',
-        component: './register',
-      },
-      {
-        path: '/inputDemo',
-        component: './inputDemo',
-      },
-    ],
+  routes: [
+    // {
+    //   path: '/',
+    //   component: './index',
+    // },
+    {
+      path: '/register',
+      component: './register',
+    },
+    {
+      path: '/inputDemo',
+      component: './inputDemo',
+    },
+    {
+      path: '/',
+      component: '../layout',
+      routes: [
+        {
+          path: 'helloworld',
+          component: './HelloWorld'
+        },
+      ]
+    }
+  ],
 };
